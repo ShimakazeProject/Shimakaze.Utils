82e5f6006d0d3fb180c2a072be1e89833dbdd3d8
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2021-02-16 13:24:22 +0800

    把结构拆出去

--- Shimakaze.Utils.Mix/Struct/MixHeader.cs
+++ Shimakaze.Utils.Mix/Struct/MixHeader.cs
@@ -18,18 +18,4 @@ namespace Shimakaze.Utils.Mix.Struct
             Files = files;
         }
     }
-
-    public struct MixIndexEntry
-    {
-        public uint Id;     // 文件校验值
-        public int Offset;  // 文件偏移
-        public int Size;    // 文件大小
-
-        public MixIndexEntry(uint id, int offset, int size)
-        {
-            Id = id;
-            Offset = offset;
-            Size = size;
-        }
-    }
 }
