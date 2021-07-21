bfae0277124cbc443de43803b1b8b7a96c43298f
舰队的偶像-岛风酱 <frg2089@outlook.com>
2021-07-21 17:28:02 +0800

    Csf Utils Sample

---
+++ sample/Shimakaze.Utils.Csf.CLI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Shimakaze.Utils.Csf;
+
+if (args.Length < 1)
+{
+    Action<string> printf = Console.WriteLine;
+    printf("<input> [<output>] [-v <byte>] [-b <int>]");
+    printf("");
+    printf("<input>  input file");
+    printf("<output>  output file (Default is <inputWithoutExtension>.Extension)");
+    printf("-b <int> Set Buffer Length (Default: 1024)");
+    printf("-v <byte> Set Version (Default use the latest)");
+
+    throw new ArgumentException("There are not enough parameters");
+}
+
+byte version = 0;
+int buffer = 1024;
+string input = string.Empty;
+string output = string.Empty;
+
+for (var i = 0; i < args.Length; i++)
+{
+    var arg = args[i];
+    if (version == 0)
+    {
+        if (arg.Equals("-v"))
+            version = byte.Parse(args[++i]);
+        else if (arg.StartsWith("-v"))
+            version = byte.Parse(arg[2..].TrimStart(':', '=', '-', '+'));
+
+        if (version != 0)
+            continue;
+    }
+    if (buffer == 1024)
+    {
+        if (arg.Equals("-b"))
+            buffer = int.Parse(args[++i]);
+        else if (arg.StartsWith("-b"))
+            buffer = int.Parse(arg[2..].TrimStart(':', '=', '-', '+'));
+
+        if (buffer != 10240)
+            continue;
+    }
+    if (string.IsNullOrEmpty(input))
+        input = arg;
+    else if (string.IsNullOrEmpty(output))
+        output = arg;
+
+}
+if (version == 0)
+    await CsfUtils.Convert(input, output, buffer);
+else
+    await CsfUtils.Convert(input, output, buffer, version);
---
+++ sample/Shimakaze.Utils.Csf.PowerShell/Program.ps1
@@ -0,0 +1,28 @@
+﻿#!/usr/bin/env pwsh
+#Requires -Version 5
+
+param($input,$output=$null,$buffer=1024,$version=0)
+if([string]::IsNullOrEmpty($input)){
+    #   printf("<input> [<output>] [-v <byte>] [-b <int>]");
+    # printf("");
+    # printf("<input>  input file");
+    # printf("<output>  output file (Default is <inputWithoutExtension>.Extension)");
+    # printf("-b <int> Set Buffer Length (Default: 1024)");
+    # printf("-v <byte> Set Version (Default use the latest)");
+    Write-Host "Usage: $PSScriptRoot\ConvertTo.ps1 <input> [<output>] [<buffer length>] [<version>]"
+    Write-Host "ConvertTo.ps1 is a tool to convert file to another format."
+    Write-Host "The default version is 0, which means the latest version."
+    Write-Host "The default buffer length is 1024."
+    Write-Host "The default output file is <inputWithoutExtension>.Extension."
+    Write-Host "this usage guide is by copilot."
+    exit
+}
+
+Import-Module Shimakaze.Utils.Csf.dll -Force
+using namespace Shimakaze.Utils.Csf;
+if ($version -eq 0){
+    CsfUtils.Convert($input, $output, $buffer).Wait();
+}
+else{
+    CsfUtils.Convert($input, $output, $buffer, $version).Wait();
+}
